5094dc33e749f8f41685dde1f142626c380d6950
take-1019 <tkchtmy@gmail.com>
2022-10-06 11:22:37 +0900

    🔧  tailwind.comfig.jsのcontentを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
+	mode: "jit",
+	content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	theme: { extend: {} },
+	plugins: [],
 };
